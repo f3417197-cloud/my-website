ecc68b6606fc3aeace59d98b600d99738a861926
f3417197-cloud <f3417197@gmail.com>
2026-07-29 17:26:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const shopBtn = document.getElementById("shopBtn");
 
+  shopBtn.addEventListener("click", () => {
+    alert("مرحباً بك في NOVA STORE 🚀");
+  });
+
+  localStorage.setItem("storeName", "NOVA STORE");
+
+  console.log("Store Loaded");
+});
