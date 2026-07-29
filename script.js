8ab64d895812c1c622ccbbfb31bbe5ed555e391b
f3417197-cloud <f3417197@gmail.com>
2026-07-29 23:59:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -43,3 +43,28 @@ text+=`${index+1}. ${item.name} - ${item.price}\n`;
 alert(text);
 
 });
+const cartPage = document.getElementById("cartPage");
+const cartItems = document.getElementById("cartItems");
+const cartTotal = document.getElementById("cartTotal");
+
+document.querySelector(".cart-icon").onclick = () => {
+    cartItems.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+        total += parseFloat(item.price);
+
+        cartItems.innerHTML += `
+        <div class="cart-item">
+            <span>${item.name}</span>
+            <span>${item.price}</span>
+        </div>`;
+    });
+
+    cartTotal.textContent = `المجموع: ${total}$`;
+    cartPage.style.display = "block";
+};
+
+document.getElementById("closeCart").onclick = () => {
+    cartPage.style.display = "none";
+};
