cbe93a753d9cfcc946b03b3d771e0d6c2a7deece
f3417197-cloud <f3417197@gmail.com>
2026-08-01 00:01:46 +0300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,41 @@
+// ===============================
+// ADMIN PANEL
+// ===============================
 
+function addProduct(product){
+
+products.push(product);
+
+renderProducts();
+
+}
+
+function deleteProduct(id){
+
+const index = products.findIndex(
+p => p.id === id
+);
+
+if(index !== -1){
+
+products.splice(index,1);
+
+renderProducts();
+
+}
+
+}
+
+function editProduct(id,newData){
+
+const product = products.find(
+p => p.id === id
+);
+
+if(!product) return;
+
+Object.assign(product,newData);
+
+renderProducts();
+
+  }
