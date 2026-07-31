d14e548321833df603c35db5434b1169fc3328de
f3417197-cloud <f3417197@gmail.com>
2026-08-01 00:17:24 +0300

    Update categories.js

--- js/categories.js
+++ js/categories.js
@@ -1,1 +1,59 @@
+const categories = [
+{
+name:"Electronics",
+icon:"💻"
+},
+{
+name:"Fashion",
+icon:"👕"
+},
+{
+name:"Gaming",
+icon:"🎮"
+},
+{
+name:"Home",
+icon:"🏠"
+},
+{
+name:"Beauty",
+icon:"💄"
+},
+{
+name:"Sports",
+icon:"⚽"
+},
+{
+name:"Books",
+icon:"📚"
+},
+{
+name:"Accessories",
+icon:"⌚"
+}
+];
 
+const categoriesContainer =
+document.getElementById("categoriesContainer");
+
+if(categoriesContainer){
+
+categories.forEach(category=>{
+
+categoriesContainer.innerHTML += `
+<div class="category-card">
+
+<div class="category-icon">
+${category.icon}
+</div>
+
+<h3 class="category-title">
+${category.name}
+</h3>
+
+</div>
+`;
+
+});
+
+  }
