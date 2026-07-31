0bceaf2a596d05be3ae46ff3826afbf1cf46edd6
f3417197-cloud <f3417197@gmail.com>
2026-07-31 20:05:32 +0300

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,1 +1,52 @@
+// ===============================
+// SEARCH SYSTEM
+// ===============================
 
+function searchProducts() {
+
+const input =
+document.getElementById("searchInput");
+
+if (!input) return;
+
+const keyword =
+input.value.toLowerCase();
+
+const cards =
+document.querySelectorAll(".product-card");
+
+cards.forEach(card => {
+
+const title =
+card.querySelector(".product-title");
+
+if (!title) return;
+
+const text =
+title.textContent.toLowerCase();
+
+if (text.includes(keyword)) {
+
+card.style.display = "block";
+
+} else {
+
+card.style.display = "none";
+
+}
+
+});
+
+}
+
+const searchInput =
+document.getElementById("searchInput");
+
+if (searchInput) {
+
+searchInput.addEventListener(
+"keyup",
+searchProducts
+);
+
+}
