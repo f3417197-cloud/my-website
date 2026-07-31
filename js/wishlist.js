0fa94e53e85518d7323a1cb5a4ae69987a5ce2a0
f3417197-cloud <f3417197@gmail.com>
2026-07-31 20:03:28 +0300

    Update wishlist.js

--- js/wishlist.js
+++ js/wishlist.js
@@ -1,1 +1,56 @@
+// ===============================
+// WISHLIST SYSTEM
+// ===============================
 
+let wishlist =
+JSON.parse(localStorage.getItem("nova_wishlist")) || [];
+
+// حفظ المفضلة
+function saveWishlist(){
+
+localStorage.setItem(
+"nova_wishlist",
+JSON.stringify(wishlist)
+);
+
+}
+
+// إضافة أو إزالة
+function toggleWishlist(id){
+
+const index =
+wishlist.indexOf(id);
+
+if(index === -1){
+
+wishlist.push(id);
+
+}else{
+
+wishlist.splice(index,1);
+
+}
+
+saveWishlist();
+
+updateWishlistCounter();
+
+}
+
+// تحديث العداد
+function updateWishlistCounter(){
+
+const counter =
+document.getElementById("favCount");
+
+if(counter){
+
+counter.textContent =
+wishlist.length;
+
+}
+
+}
+
+// تشغيل العداد
+updateWishlistCounter();
