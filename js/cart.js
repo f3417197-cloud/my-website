3ae67f7d8dde61cc4bfad66809e6a4a8751ea455
f3417197-cloud <f3417197@gmail.com>
2026-07-31 20:01:41 +0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,51 @@
+// ===============================
+// CART SYSTEM
+// ===============================
 
+let cart = JSON.parse(localStorage.getItem("nova_cart")) || [];
+
+// حفظ السلة
+function saveCart() {
+    localStorage.setItem("nova_cart", JSON.stringify(cart));
+}
+
+// إضافة منتج
+function addToCart(id) {
+
+    const product = products.find(p => p.id === id);
+
+    if (!product) return;
+
+    cart.push(product);
+
+    saveCart();
+
+    updateCartCounter();
+
+    alert(product.name + " added to cart");
+}
+
+// تحديث العداد
+function updateCartCounter() {
+
+    const counter = document.getElementById("cartCount");
+
+    if (counter) {
+        counter.textContent = cart.length;
+    }
+
+}
+
+// حذف منتج
+function removeFromCart(id) {
+
+    cart = cart.filter(item => item.id !== id);
+
+    saveCart();
+
+    updateCartCounter();
+
+}
+
+// تشغيل العداد عند فتح الموقع
+updateCartCounter();
